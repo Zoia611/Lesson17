cf0c2fc1d5599116f2921bbb326817c644fa6ad1
Zoia611 <117438266+Zoia611@users.noreply.github.com>
2022-11-27 11:49:01 +0300

    Добавьте файлы проекта.

---
+++ Task1/BankCheck.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class BankCheck
+    {
+         A number;
+        double balance;
+        string name;
+
+        public A Number
+        {
+            get
+            {
+                return number;
+            }
+            set
+            {
+                number = value;
+            }
+        }
+        public double Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                balance = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string GetInfo()
+        {
+            return $"{Number} Остаток по счету: {Balance}тыс$ ФИО: {Name}";
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankCheck<int> BС1 = new BankCheck<int>();
+            BС1.Number = 1;
+            BС1.Balance = 300;
+            BС1.Name = "Иванов Иван";
+            Console.WriteLine(BС1.GetInfo());
+
+            BankCheck<string> BС2 = new BankCheck<string>();
+            BС2.Number = 2;
+            BС2.Balance = 250;
+            BС2.Name = "Петров Петр";
+            Console.WriteLine(BС2.GetInfo());
+
+            BankCheck<double> BС3 = new BankCheck<double>();
+            BС3.Number = 3;
+            BС3.Balance = 150;
+            BС3.Name = "Викторов Виктор";
+            Console.WriteLine(BС3.GetInfo());
+
+            Console.ReadKey();
+        }
+
+        private class BankCheck<T>
+        {
+            internal bool GetInfo()
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
